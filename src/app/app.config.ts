63386072147fcec56a9942f87f3a313b71b0ad17
Alvaro <martinhuertaalvaro@gmail.com>
2024-04-01 23:24:22 +0200

    fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,20 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    provideNzI18n(en_US),
-    importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
     provideHttpClient(),
   ],
