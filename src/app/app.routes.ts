c0e79fb4fa543099bcedf55dd5f96745707b1a2a
Alvaro <martinhuertaalvaro@gmail.com>
2024-04-15 18:52:55 +0200

    create user/profile and user/settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './modules/auth/layout/auth/auth.component';
 import { LayoutComponent } from './modules/core/layout/layout.component';
 import { MainComponent } from './modules/core/views/main/main.component';
+import { ProfileComponent } from './modules/core/views/user/profile/profile.component';
+import { SettingsComponent } from './modules/core/views/user/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
@@ -10,8 +12,16 @@ export const routes: Routes = [
     path: 'core',
     component: LayoutComponent,
     children: [
-      { path: 'main', component: MainComponent },
       { path: '', redirectTo: 'main', pathMatch: 'full' },
+      { path: 'main', component: MainComponent },
+      {
+        path: 'user',
+        children: [
+          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+          { path: 'profile', component: ProfileComponent },
+          { path: 'settings', component: SettingsComponent },
+        ],
+      },
     ],
   },
 ];
