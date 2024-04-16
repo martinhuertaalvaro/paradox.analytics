a290f1a10f6786627b4933c0b0b27478087d219e
Alvaro Martin <amartinh@hiberus.com>
2024-04-16 16:50:16 +0200

    layout for analytics

--- src/app/modules/core/views/analytics/analytics.component.ts
+++ src/app/modules/core/views/analytics/analytics.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { ZorroNgModule } from '../../../ng-zorro/zorro-ng.module';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [ZorroNgModule],
   templateUrl: './analytics.component.html',
-  styleUrl: './analytics.component.scss'
+  styleUrl: './analytics.component.scss',
 })
 export class AnalyticsComponent {
+  chart: any = [];
 
+  constructor() {}
+
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['Wins', 'Loses'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    });
+  }
 }
