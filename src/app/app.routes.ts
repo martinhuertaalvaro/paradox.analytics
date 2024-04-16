b1fb4198832dad008addb8de448c2ee89c0ca1ff
Alvaro Martin <amartinh@hiberus.com>
2024-04-16 11:43:50 +0200

    creating pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LayoutComponent } from './modules/core/layout/layout.component';
 import { MainComponent } from './modules/core/views/main/main.component';
 import { ProfileComponent } from './modules/core/views/user/profile/profile.component';
 import { SettingsComponent } from './modules/core/views/user/settings/settings.component';
+import { AnalyticsComponent } from './modules/core/views/analytics/analytics.component';
+import { DevicesComponent } from './modules/core/views/devices/devices.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
@@ -13,14 +15,49 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: '', redirectTo: 'main', pathMatch: 'full' },
-      { path: 'main', component: MainComponent },
+      {
+        path: 'main',
+        component: MainComponent,
+        data: {
+          customBreadcrumb: 'Home',
+        },
+      },
       {
         path: 'user',
+        data: {
+          customBreadcrumb: 'User',
+        },
         children: [
           { path: '', redirectTo: 'profile', pathMatch: 'full' },
-          { path: 'profile', component: ProfileComponent },
-          { path: 'settings', component: SettingsComponent },
+          {
+            path: 'profile',
+            component: ProfileComponent,
+            data: {
+              customBreadcrumb: 'Profile',
+            },
+          },
+          {
+            path: 'settings',
+            component: SettingsComponent,
+            data: {
+              customBreadcrumb: 'Settings',
+            },
+          },
         ],
+      },
+      {
+        path: 'analytics',
+        component: AnalyticsComponent,
+        data: {
+          customBreadcrumb: 'Analytics',
+        },
+      },
+      {
+        path: 'devices',
+        component: DevicesComponent,
+        data: {
+          customBreadcrumb: 'Devices',
+        },
       },
     ],
   },
--- src/app/modules/core/layout/layout.component.ts
+++ src/app/modules/core/layout/layout.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ZorroNgModule } from '../../ng-zorro/zorro-ng.module';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, ZorroNgModule, RouterLink, RouterOutlet],
+  imports: [
+    CommonModule,
+    ZorroNgModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
