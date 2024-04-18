fa92a974ba341c887effce96d524bc262f38a5b5
Alvaro <martinhuertaalvaro@gmail.com>
2024-04-18 19:47:44 +0200

    #1 check device call and add token interceptorr

--- src/app/modules/core/pages/user/views/profile/profile.component.ts
+++ src/app/modules/core/pages/user/views/profile/profile.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  private userSvc = inject(UserService);
 
+  async ngOnInit() {
+    const user = await lastValueFrom(this.userSvc.getAllTenants());
+    console.log(user);
+  }
 }
