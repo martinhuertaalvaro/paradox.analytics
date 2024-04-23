3fe616137e71dc91121e8b3c920950fb9893eb9c
Alvaro <martinhuertaalvaro@gmail.com>
2024-04-23 11:57:27 +0200

    #1 add florida styles

--- src/app/modules/core/pages/user/services/user.service.ts
+++ src/app/modules/core/pages/user/services/user.service.ts
@@ -9,4 +9,14 @@ export class UserService extends BaseService {
   getAllTenants(): Observable<any> {
     return this.http.get<any>(`${this.baseUrlMaestros}/device/all`);
   }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrlMaestros}/user/all`);
+  }
+
+  getUserInfo(email: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrlMaestros}/user/info`, {
+      params: { email: email },
+    });
+  }
 }
