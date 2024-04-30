c599f841729d64ce8cab449b371c8eec00e658f6
Álvaro Martín Huerta <114902322+martinhuertaalvaro@users.noreply.github.com>
2024-04-30 15:55:11 +0200

    change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,85 +1,85 @@
-import { ActivatedRoute, Routes } from '@angular/router';
-import { AuthComponent } from './modules/auth/layout/auth/auth.component';
-import { LayoutComponent } from './modules/core/layout/layout.component';
-import { MainComponent } from './modules/core/pages/main/main.component';
-import { ProfileComponent } from './modules/core/pages/user/views/profile/profile.component';
-import { SettingsComponent } from './modules/core/pages/settings/settings.component';
-import { AnalyticsComponent } from './modules/core/pages/analytics/analytics.component';
-import { DevicesComponent } from './modules/core/pages/devices/devices.component';
-import { coreGuard } from './modules/shared/guards/core.guard';
-import { authGuard } from './modules/shared/guards/auth.guard';
-import { ManagementComponent } from './modules/core/pages/user/views/management/management.component';
+import { ActivatedRoute, Routes } from "@angular/router";
+import { AuthComponent } from "./modules/auth/layout/auth/auth.component";
+import { LayoutComponent } from "./modules/core/layout/layout.component";
+import { MainComponent } from "./modules/core/pages/main/main.component";
+import { ProfileComponent } from "./modules/core/pages/user/views/profile/profile.component";
+import { SettingsComponent } from "./modules/core/pages/settings/settings.component";
+import { AnalyticsComponent } from "./modules/core/pages/analytics/analytics.component";
+import { DevicesComponent } from "./modules/core/pages/devices/devices.component";
+import { coreGuard } from "./modules/shared/guards/core.guard";
+import { authGuard } from "./modules/shared/guards/auth.guard";
+import { ManagementComponent } from "./modules/core/pages/user/views/management/management.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent, canActivate: [authGuard] },
-  {
-    path: 'core',
-    component: LayoutComponent,
-    canActivate: [coreGuard],
-    children: [
-      { path: '', redirectTo: 'main', pathMatch: 'full' },
-      {
-        path: 'main',
-        component: MainComponent,
-        data: {
-          customBreadcrumb: 'Home',
-        },
-      },
-      {
-        path: 'user',
-        data: {
-          customBreadcrumb: 'User',
-        },
+    { path: "", redirectTo: "auth", pathMatch: "full" },
+    { path: "auth", component: AuthComponent, canActivate: [authGuard] },
+    {
+        path: "core",
+        component: LayoutComponent,
+        canActivate: [coreGuard],
         children: [
-          { path: '', redirectTo: 'management', pathMatch: 'full' },
-          {
-            path: 'management',
-            component: ManagementComponent,
-            data: {
-              customBreadcrumb: 'Management',
+            { path: "", redirectTo: "main", pathMatch: "full" },
+            {
+                path: "main",
+                component: MainComponent,
+                data: {
+                    customBreadcrumb: "Home",
+                },
             },
-          },
-          {
-            path: 'profile',
-            component: ProfileComponent,
-            data: {
-              customBreadcrumb: 'My Profile',
+            {
+                path: "user",
+                data: {
+                    customBreadcrumb: "User",
+                },
+                children: [
+                    { path: "", redirectTo: "management", pathMatch: "full" },
+                    {
+                        path: "management",
+                        component: ManagementComponent,
+                        data: {
+                            customBreadcrumb: "Management",
+                        },
+                    },
+                    {
+                        path: "profile",
+                        component: ProfileComponent,
+                        data: {
+                            customBreadcrumb: "My Profile",
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-      {
-        path: 'settings',
-        component: SettingsComponent,
-        data: {
-          customBreadcrumb: 'Settings',
-        },
-      },
-      {
-        path: 'analytics',
-        component: AnalyticsComponent,
-        data: {
-          customBreadcrumb: 'Analytics',
-        },
-      },
-      {
-        path: 'devices',
-        data: {
-          customBreadcrumb: 'Devices',
-        },
-        children: [
-          { path: '', redirectTo: '/home', pathMatch: 'full' },
+            {
+                path: "settings",
+                component: SettingsComponent,
+                data: {
+                    customBreadcrumb: "Settings",
+                },
+            },
+            {
+                path: "analytics",
+                component: AnalyticsComponent,
+                data: {
+                    customBreadcrumb: "Analytics",
+                },
+            },
+            {
+                path: "devices",
+                data: {
+                    customBreadcrumb: "Devices",
+                },
+                children: [
+                    { path: "", redirectTo: "/home", pathMatch: "full" },
 
-          {
-            path: ':name',
-            component: DevicesComponent,
-            data: {
-              customBreadcrumb: 'Details',
+                    {
+                        path: ":name",
+                        component: DevicesComponent,
+                        data: {
+                            customBreadcrumb: "Details",
+                        },
+                    },
+                ],
             },
-          },
         ],
-      },
-    ],
-  },
+    },
 ];
