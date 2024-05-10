cf652d901ba590914f985520aac7e942ab9fd603
Alvaro <martinhuertaalvaro@gmail.com>
2024-05-10 20:49:44 +0200

    condicion

--- src/app/modules/core/pages/user/views/profile/profile.component.ts
+++ src/app/modules/core/pages/user/views/profile/profile.component.ts
@@ -5,11 +5,12 @@ import { AuthService } from '../../../../../auth/services/auth.service';
 import { User } from '../../interfaces/user';
 import { ZorroNgModule } from '../../../../../ng-zorro/zorro-ng.module';
 import { PrimeNgModule } from '../../../../../ng-prime/prime-ng.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [ZorroNgModule, PrimeNgModule],
+  imports: [ZorroNgModule, PrimeNgModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
@@ -17,9 +18,11 @@ export class ProfileComponent {
   protected userSvc = inject(UserService);
   protected authSvc = inject(AuthService);
   public user: User = {};
+  public show: boolean = false;
   async ngOnInit() {
     this.user = await lastValueFrom(
       this.userSvc.getUserInfo(this.authSvc.getUserFromAccesToken())
     );
+    this.show = true;
   }
 }
